afeca36c44ab3545b57566658d5f491b0d712a39
Kaleena Dybas <kaleena.m.dybas@gmail.com>
2025-08-06 23:54:18 -0400

    adding js pusedo code for recipe fetching; hooking up html to js script;

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Hello from the otter sideeeeeee");
+
+// Hook into the form input to get the users input value to use for our api call
+// Save the users input
+// Validate that the input doesn't have numbers and no leading or trailing spaces
+// If there is no input, then we don't call the api
+// If there is an input, we use that in our api call to find recipes.
+
+// After we have our api recipe data, we will use that data to display the recipes found.
+// We will loop through the recipes
+// For each recipe, 
+// we will display the name
+// we will display a picture
+// we will display the ingredients
+// we will display the missing ingredients
+// we will display the recipe.
+
+// Stretch goal:
+// ability to convert recipe from cups to grams and back
