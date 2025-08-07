8147a12b1cd0ee543131b047543b52a3c5752e31
Kaleena Dybas <kaleena.m.dybas@gmail.com>
2025-08-07 01:14:02 -0400

    Coding out initial pusedo code;

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 console.log("Hello from the otter sideeeeeee");
 
 // Hook into the form input to get the users input value to use for our api call
+const inputElement = document.getElementById("ingredients");
+console.log(inputElement);
 // Save the users input
+const userInput = inputElement.innerText;
+console.log(userInput)
 // Validate that the input doesn't have numbers and no leading or trailing spaces
+userInput.trim()
+console.log(userInput)
 // If there is no input, then we don't call the api
+if (!userInput.length) {
+  console.log("Inside this quick exit")
+  // return;
+}
 // If there is an input, we use that in our api call to find recipes.
+fetchRecipes(userInput)
+
+function fetchRecipes(ingredients) {
+  // fetch spoonacular recipes
+  console.log(ingredients)
+} 
+
 
 // After we have our api recipe data, we will use that data to display the recipes found.
 // We will loop through the recipes
@@ -17,3 +34,31 @@ console.log("Hello from the otter sideeeeeee");
 
 // Stretch goal:
 // ability to convert recipe from cups to grams and back
+// number validation on input
+
+
+
+
+
+
+
+
+
+
+
+
+// Event listening practice from MDN docs
+// function changeColor(newColor) {
+//   console.log("newColor", newColor)
+//   const elem = document.getElementById("para");
+//   elem.style.color = newColor;
+// }
+
+// document.querySelectorAll("button").forEach((button) => {
+//   console.log(button)
+//   button.addEventListener("click", (event) => {
+//     console.log(event)
+//     console.log(event.target.textContent)
+//     changeColor(event.target.textContent.toLowerCase());
+//   });
+// });
