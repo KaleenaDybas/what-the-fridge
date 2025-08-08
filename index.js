fe4075de756d4323912a9fef7c7da17f0fa69a28
Kaleena Dybas <kaleena.m.dybas@gmail.com>
2025-08-08 00:43:29 -0400

    Removing some console logs and psuedo-code comments; renaming some functions to better match what they do;

--- index.js
+++ index.js
@@ -1,79 +1,57 @@
-console.log("Hello from the otter sideeeeeee");
 const SPOONACULAR_API_KEY = '';
 
-async function asyncStuff(ingredients) {
+const recipesContainer = document.getElementById("recipes-container");
+const formSubmission = document.getElementById('ingredient-form')
+formSubmission.addEventListener("submit", handleFetchingRecipes)
+
+function appendTextToRecipeContParagraph(text) {
+  const paraTag = document.getElementById("para-tag")
+  paraTag.innerText = text
+}
+
+async function fetchRecipes(ingredients) {
   const response = await fetch(
     `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients
     }&number=1&apiKey=${SPOONACULAR_API_KEY}`
   );
-  console.log("RESPONSE")
-  console.log(response)
+  const data = await response.json();
+
+  if (!data.length) {
+    appendTextToRecipeContParagraph("No recipes found.");
+    return;
+  }
+
+  displayRecipeCard(fakeAPIResponse)
 }
 
-function handleRecipe(e) {
+function handleFetchingRecipes(e) {
   e.preventDefault()
-  console.log("SUBMIT BUTTON CLICKED!! INSIDE HANDLE FUNCTION")
-  // Hook into the form input to get the users input value to use for our api call
-  const inputElement = document.getElementById("ingredients");
-  console.log(inputElement);
-  // Save the users input
-  const userInput = inputElement.innerText;
-  console.log(userInput)
-  // Validate that the input doesn't have numbers and no leading or trailing spaces
+  const inputedIngredients = document.getElementById("ingredients");
+  const userInput = inputedIngredients.value;
   userInput.trim()
-  console.log(userInput)
-  // If there is no input, then we don't call the api
+
   if (!userInput.length) {
-    console.log("Inside this quick exit")
-    // return;
+    return;
   }
-  // If there is an input, we use that in our api call to find recipes.
+
+  appendTextToRecipeContParagraph("Searching Recipes...")
   fetchRecipes(userInput)
 }
 
 
-function fetchRecipes(ingredients) {
-  // fetch spoonacular recipes
-  console.log('ingredients in fetch', ingredients)
-  // asyncStuff(ingredients)
-  console.log(fakeAPIResponse)
-  displayRecipeCard(fakeAPIResponse)
-  return fakeAPIResponse;
-}
-
-// DONE - All the console logs above are getting put in the dev console each time the page loads right now
-// I need to make sure they are only called when I click the submit button or I'll run out 
-// of my free spoonacular api stuff/token?
-
-// hook into the form submit button
-const submitButton = document.getElementById('ingredient-form')
-// add event listener to submit
-submitButton.addEventListener("submit", handleRecipe)
-// when clicked, then do all the input stuff above
-
-
-
-// After we have our api recipe data, we will use that data to display the recipes found.
 function displayRecipeCard(recipes) {
-  console.log("INSIDE DISPLAY")
-  const recipesContainer = document.getElementById("recipes-container");
-
-  // We will loop through the recipes
+  
   recipes.forEach(recipe => {
     console.log(recipe)
-    // For each recipe, 
-    // we will display the name
+
     const name = document.createElement("h3")
     name.innerText = recipe.title
 
-    // we will display a picture
     const img = document.createElement("img")
     img.src = recipe.image
     img.alt = recipe.title
 
-    // we will display the ingredients
-    // we will display the missing ingredients
-    // we will display the recipe.
+
     recipesContainer.append(name);
     recipesContainer.append(img);
   })
